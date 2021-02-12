bab24f63848fd8c033553324e7892588ef564720
alaa saleh <alaaasaleh7@gmail.com>
2021-02-12 02:58:26 +0200

    s

--- AbnsinaiPharma/Controllers/ReportController.cs
+++ AbnsinaiPharma/Controllers/ReportController.cs
@@ -1,7 +1,9 @@
-﻿using AbnsinaiPharma.IServices;
+﻿using AbnsinaiPharma.DTO;
+using AbnsinaiPharma.IServices;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using static System.Net.WebRequestMethods;
 
 namespace AbnsinaiPharma.Controllers
 {
@@ -15,29 +17,17 @@ namespace AbnsinaiPharma.Controllers
             _reportService = reportService;
 
         }
-        [HttpGet]
-        public IActionResult GetStockReportFile(string fileType, string fileName)
-        
-        {
-            var report = _reportService.GetReport("stock", "Daily_Sales_and_Stock_for_AL_ANDALOUS20210110080017.xlsx");
-            if(report == null)
-            {
-                return BadRequest();
-            }
-            else
-            {
-                return Ok(report);
-            }
-        }
+
 
 
         [HttpGet("downloadFile")]
-        public FileStream downloadFile(string fileName)
+        public FileStream downloadFile(string FileName, string ReportType)
+        {
         {
             try
             {
-                var currentDirectory = @"wwwroot\stock\";
-                var file = Path.Combine(currentDirectory, fileName);
+                var currentDirectory = string.Concat(@"wwwroot\" ,ReportType,"/");
+                var file = Path.Combine(currentDirectory,FileName);
                 return new FileStream(file, FileMode.Open, FileAccess.Read);
             }
             catch (Exception e)
@@ -47,4 +37,5 @@ namespace AbnsinaiPharma.Controllers
             }
         }
     }
+    
 }
